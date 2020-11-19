bb483aaefaf0348d9afaf2812906a2df492788e5
Pedro Dias <pedrofelipe18@hotmail.com.br>
2020-11-18 21:55:38 -0300

    Camera finalizada

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, TouchableOpacity, Button, Image } from 'react-native';
+import { Camera } from 'expo-camera';
+
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  // const [previewVisible, setPreviewVisible] = useState(false)
+  // const [capturedImage, setCapturedImage] = useState(null)
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [imagemUri, SetImagemUri] = useState(null);
+
+  let camera = Camera;
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  let snap = async () => {
+    if (camera){
+      let photo = await camera.takePictureAsync();
+      alert('Foto tirada!')
+      console.log(photo);
+
+      SetImagemUri(photo.uri);
+    }
+  }
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <Camera style={{ flex: 1 }} type={type} 
+       ref={ref => {
+          camera = ref;
+      }}
+      >
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+          }}>
+          <TouchableOpacity
+            style={{
+              flex: 0.1,
+              alignSelf: 'flex-end',
+              alignItems: 'center',
+            }}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+            
+          </TouchableOpacity>
+          
+        </View>
+      </Camera>
+        <Image source={{uri : imagemUri}} style={{height : 300}} />
+        <Button title="Tirar foto" onPress={() => snap()}/>
     </View>
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
